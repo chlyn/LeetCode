424cf1b2ebf1ff5672028c139ae4562dec8a6613
Chenilyn Joy Espineda <chlynespined@gmail.com>
2024-09-02 11:26:32 -0400

    SOLVED: 1768_Merge_Strings_Alternately.py

---
+++ python/1768_Merge_Strings_Alternately.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=1768 lang=python
+#
+# [1768] Merge Strings Alternately
+#
+
+# @lc code=start
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+
+        result = []
+        i = 0
+
+        while i < (len(word1) + len(word2)):
+            if i < len(word1):  
+                result.append(word1[i])
+
+            if i < len(word2):
+                result.append(word2[i])
+            
+            i += 1
+
+        return ''.join(result)
+        
+# @lc code=end
+
