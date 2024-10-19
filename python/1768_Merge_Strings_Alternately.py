5d26aa3c3789f594f73e32d8a049160312f239fb
Chenilyn Joy Espineda <chlynespined@gmail.com>
2024-10-19 17:51:05 -0400

    Upddate: 1768_Merge_Strings_Alternately

--- python/1768_Merge_Strings_Alternately.py
+++ python/1768_Merge_Strings_Alternately.py
@@ -9,15 +9,12 @@ class Solution(object):
         """
 
         result = []
-        i = 0
 
-        while i < (len(word1) + len(word2)):
+        for i in range (len(word1) + len(word2)):
             if i < len(word1):  
                 result.append(word1[i])
 
             if i < len(word2):
                 result.append(word2[i])
-            
-            i += 1
 
         return ''.join(result)
