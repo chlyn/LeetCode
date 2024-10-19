23f4ace3abf3ce3788f58c897150415718417ec3
Chenilyn Joy Espineda <chlynespined@gmail.com>
2024-10-19 18:25:26 -0400

    Added: 1768_Merge_Strings_Alternately.java

---
+++ java/1768_Merge_Strings_Alternately.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        
+        ArrayList<String> result = new ArrayList<String>();
+        int size = word1.length() + word2.length();
+
+        for (int i = 0; i < size; i++) {
+            if (i < word1.length()) {
+                char letter = word1.charAt(i);
+                result.add(Character.toString(letter));
+            }
+
+            if (i <  word2.length()) {
+                char letter = word2.charAt(i);
+                result.add(Character.toString(letter));
+            }
+        }
+
+        return String.join("", result);
+    }
+}
